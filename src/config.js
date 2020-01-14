5595678a45562afe99840caee8cadbc6e861363a
Zarema <zarema.klva@gmail.com>
2020-01-14 16:16:03 +0300

    Add config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "ROOT": __dirname,
+    "CATEGORIES": ['frontend', 'backend',
+        'machine-learning',
+        'dependency-inversion',
+        'computer-graphics', 'Algorithms'],
+    "PORT": 3002,
+    "URL": 'localhost'
+}
