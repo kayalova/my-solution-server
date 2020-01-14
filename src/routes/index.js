14e71af50e2bd4009ceffa4ca68bdb879fbf4a35
Zarema <zarema.klva@gmail.com>
2020-01-14 16:25:31 +0300

    Completed routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,61 +1,65 @@
 const router = require('express').Router()
+const mongoose = require('mongoose')
 const Snippet = require('../models/snippet')
 const {
-    filterSnippetsBySelectors,
+    filterSnippets,
     buildCodePreview,
-    formatDate,
-    buildFile } = require('../helpers')
+    getSnippetPath,
+    buildFile,
+    deleteFile } = require('../helpers')
 
-router.post('/snippets', (req, res) => {
 
+router.get('/snippets/search', async (req, res) => {
+    const { filename: userFilename, ...filters } = req.query
     const selectors = {}
-    for (let [field, value] of Object.entries(req.body)) {
+    for (let [field, value] of Object.entries({ userFilename, ...filters })) {
         if (value)
             selectors[field] = value
     }
 
-    res.send(['hello again'])
-    filterSnippetsBySelectors(selectors)
+    const snippets = await filterSnippets(Snippet, selectors)
+    res.send(JSON.stringify(snippets))
 })
 
-
 router.post('/snippets/create', async (req, res) => {
-    const { filename: userFilename, category, code } = req.body
-    const description = buildCodePreview(code)
-    const date = formatDate()
+    console.log(req.body)
+    const { filename: userFilename, category, code, description } = req.body
+    const codePreview = buildCodePreview(code)
+    const createdDate = new Date().getTime()
+    const _id = new mongoose.Types.ObjectId()
+    const filename = `${_id}-${userFilename}`
+    buildFile(filename, code)
 
-    const filename = '1-App.js'
-    //создать файл и поместить его в папку files 
-    const f = buildFile(filename, code)
-
-
-    //создание экземпляра модели сниппета
     const snippet = new Snippet({
-        _id: new mongoose.Types.ObjectId(),
-        file: `${_id}-${userFilename}`,
-        pathToFile: '',
+        _id,
+        filename,
+        pathToFile: getSnippetPath(filename),
         userFilename,
         description,
+        codePreview,
         category,
-        date,
+        createdDate,
     })
-
-    // сохраняем экземпляр
     try {
-        const newSnippet = await snippet.save()
-        res.status(201).json(newSnippet)
+        await snippet.save()
+        Snippet.find({}).sort({ createdDate: 'desc' }).exec(function (err, snippets) {
+            if (err) console.log(err)
+            res.status(201).send(snippets)
+        });
     } catch (err) {
         res.status(400).json({ message: err.message })
     }
-
-    //делаем редирект на /api/snippets
 })
 
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/snippets/:id', (req, res) => {
+    Snippet.findByIdAndRemove({ _id: req.params.id }, (err, snippet) => {
+        if (err) console.log(err)
+        deleteFile(snippet.pathToFile)
+        res.status(200)
+    })
+    console.log(req.params.id)
 })
-
 
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,40 +1,48 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const api = require('./routes/index')
-const Snippet = require('./models/snippet')
-const port = 3002
-const url = 'localhost'
-const app = express()
+const bodyParser = require('body-parser')
+const api = require('./routes')
+const { URL, PORT } = require('./config')
 
-mongoose.connect('mongodb://localhost:27017/snippets', { useNewUrlParser: true })
+const app = express()
+const cors = require('cors');
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/snippets',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(PORT, URL, (err) => {
+            if (err) {
+                console.log('Error: ' + err)
+                return
+            }
+
+            console.log('Server started')
+        })
+    })
+
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
-db.on('open', () => console.log('connected to database'))
+db.on('open', () => {
+    console.log('connected to database')
 
+})
 
-/* --------- test initial data ---------- */
-const data = require('./data/index')
-const first = new Snippet(data[0]).save()
-const second = new Snippet(data[1]).save()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
-app.use(express.json())
-app.use('/api', api)
-
-
-app.listen(port, url, (err) => {
-    if (err) {
-        console.log('Error: ' + err)
-        return
-    }
-
-    console.log('Server started')
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    next()
 })
+
+app.use('/api', api)
