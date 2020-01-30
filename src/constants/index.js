bf7248c1f6ccabe941bf8f7df22681849b4bb9d9
Zarema <zarema.klva@gmail.com>
2020-01-30 09:13:10 +0300

    Add error constants

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+const ERROR_MSG = {
+    SNIPPET: {
+        CREATE_FAILED: 'Не удалось создать сниппет',
+        REMOVE_FAILED: 'Не удалось удалить сниппет',
+        FIND_FAILED: 'Не удалоь найти сниппеты',
+    }
+}
+
+module.exports = {
+    ERROR_MSG,
+}
